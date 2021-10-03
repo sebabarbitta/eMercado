566d2b7fc5e0919bc506e929e16751aac88d4bfa
sebabarbitta <sebabarbitta@hotmail.com>
2021-10-03 20:38:41 -0300

    Productos relacionados v2

--- js/product-info.js
+++ js/product-info.js
@@ -13,33 +13,40 @@ function imagenes(img){
   
 
     for(let i = 0; i < img.length; i++){
-      // let  imagen = img[i];
+       let  imagen = img[i];
 
         verimg = `
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-  <div class="carousel-item active">
-  <img src="`+img[0]+`" class="d-block w-20" height="300px">
-</div>
-    <div class="carousel-item">
-    <img src="`+img[1]+`" class="d-block w-20" height="200px">
-  </div>
-  <div class="carousel-item" >
-    <img src="`+img[2]+`" class="d-block w-20" height="200px">
-  </div>
-  <div class="carousel-item" >
-    <img src="`+img[3]+`" class="d-block w-20" height="200px">
-  </div>
-
-   
-    
-  </div>
-</div>
+      
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+           <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="`+img[0]+`" class="d-block w-10" width="450px" height="200px">
+              </div>
+              <div class="carousel-item">
+                <img src="`+img[1]+`" class="d-block w-10" width="450px" height="200px">
+              </div>
+              <div class="carousel-item">
+                <img src="`+img[2]+`" class="d-block w-10" width="450px" height="200px">
+              </div>
+              <div class="carousel-item">
+                <img src="`+img[3]+`" class="d-block w-10" width="450px" height="200px">
+              </div>
+           </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+     
         `
 
         
     }
-    document.getElementById("verimagen").innerHTML = verimg;
+    document.getElementById("verimagen").innerHTML += verimg;
 }
 
 function estrellas(estre){
